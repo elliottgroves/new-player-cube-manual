e167ada680f9440f1cf9241ed72e0bca39b3e625
Elliott Groves <sevorge@gmail.com>
2024-07-12 17:03:09 -0400

    Book animation prototype.

--- src/book.js
+++ src/book.js
@@ -3,9 +3,14 @@ import './book.css';
 
 export default function Book() {
   const [open, setOpen] = useState(false);
+  const [pageTurned, setPageTurned] = useState(false);
 
   function toggleOpen() {
     setOpen(!open);
+  }
+
+  function togglePageTurned() {
+    setPageTurned(!pageTurned);
   }
 
   return (
@@ -14,9 +19,13 @@ export default function Book() {
         <h2 className="title">Card Types</h2>
         <span className="tap-text">(tap to open)</span>
       </div>
-      <div className="page-bg">
+      <div onClick={togglePageTurned} className={"page-fg" + (pageTurned ? ' turned' : '')}>
         <h3>Creature Cards</h3>
         <img className="card" src="https://api.scryfall.com/cards/named?exact=Grizzly+Bears&amp;format=image&amp;version=normal" />
+      </div>
+      <div className="page-bg">
+        <h3>Land Cards</h3>
+        <img className="card" src="https://api.scryfall.com/cards/named?exact=Meandering+River&amp;format=image&amp;version=normal" />
       </div>
     </div>
   );
