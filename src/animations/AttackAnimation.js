baad8730b0cd45d5c343f98c034d24e1c410e0a1
Elliott Groves <sevorge@gmail.com>
2024-07-17 11:54:08 -0400

    Motion one refactor start.

--- src/animations/AttackAnimation.js
+++ src/animations/AttackAnimation.js
@@ -2,9 +2,35 @@ import GrizzlyBears from '../images/Grizzly Bears.webp';
 import CentaurCourser from '../images/Centaur Courser.webp';
 import Forest from '../images/Forest.webp';
 import Mountain from '../images/Mountain.webp';
+import { animate, inView } from 'motion';
+import { useEffect } from 'react';
 import './attackanimation.css';
 
+
 export default function AttackAnimation() {
+  useEffect(() => {
+    inView('.attack-animation', fullAnimation);
+
+    function fullAnimation() {
+      const bear = animate('.bear', 
+        { transform: [
+          'translateY(0) rotate(0)', 
+          'translateY(0) rotate(90deg)',
+          'translateY(0) rotate(90deg)',
+          'translateY(-75px) rotate(90deg)',
+          'translateY(-75px) rotate(90deg)',
+          'translateY(0) rotate(0)'
+        ] },
+        { offset: [0, 0.2, 0.25, 0.4, 0.7, 1], 
+          easing: ['ease'],
+          duration: 4, 
+          repeat: Infinity }
+      );
+
+      return (leaveInfo) => bear.stop();
+    }
+  }, []);
+
   return (
     <figure className="attack-animation">
       <div className="creatures">
